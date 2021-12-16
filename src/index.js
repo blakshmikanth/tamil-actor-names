3f91836fb848b5bf8b9430ac86976bd008294665
Lakshmikanth Balasundaram <lakshmikanth@contactwise.io>
2021-12-16 15:06:21 +0530

    Adding the library

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import uniqueRandomArray from "unique-random-array";
+// import names from "./names.json";
+import uniqueRandomArray from "unique-random-array";
+const names = ["Rajinikanth", "Kamal Haasan", "Vijay", "Ajith"];
+
+export const all = names;
+export const random = uniqueRandomArray(names);
---
+++ src/names.js
@@ -0,0 +1,8 @@
+export default names = [
+  "M G Ramachandran",
+  "Sivaji Ganesan",
+  "Rajinikanth",
+  "Kamal Haasan",
+  "Vijay",
+  "Ajith",
+];
